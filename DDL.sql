1898a720aa186c0a15c5a8d952675b4fb8978986
Yousef Kotp <yousefkotp@outlook.com>
2022-12-25 22:44:19 +0200

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -2,36 +2,50 @@ CREATE SCHEMA `car-rental-system`;
 USE `car-rental-system`;
 
 CREATE TABLE customer (
-    ssn CHAR(14),
-    fname VARCHAR(256) NOT NULL,
-    lname VARCHAR(256) NOT NULL,
-    email VARCHAR(256) UNIQUE NOT NULL,
-    phone_no VARCHAR(256) UNIQUE NOT NULL,
+    ssn CHAR(6),
+    fname VARCHAR(32) NOT NULL,
+    lname VARCHAR(32) NOT NULL,
+    email VARCHAR(64) UNIQUE NOT NULL,
+    phone_no CHAR(11) UNIQUE NOT NULL,
     password VARCHAR(256) NOT NULL,
     wallet REAL DEFAULT 0,
-    holder_name VARCHAR(256) NOT NULL,
-    card_no VARCHAR(256) NOT NULL,
-    cvv INT NOT NULL,
-    exp_date DATE NOT NULL,
+    card_no CHAR(16),
     CONSTRAINT customer_pk PRIMARY KEY (ssn)
+    CONSTRAINT customer_credit_card_fk FOREIGN KEY (card_no) REFERENCES credit_card(card_no)
+);
+
+CREATE TABLE credit_card(
+    card_no CHAR(16),
+    holder_name VARCHAR(64) NOT NULL,
+    cvv CHAR(3) NOT NULL,
+    exp_date DATE NOT NULL,
+    CONSTRAINT credit_card_pk PRIMARY KEY (card_no)
+);
+
+CREATE TABLE customer_credit(
+    ssn CHAR(6),
+    card_no CHAR(16),
+    CONSTRAINT customer_credit_pk PRIMARY KEY (ssn, card_no),
+    CONSTRAINT customer_credit_customer_fk FOREIGN KEY (ssn) REFERENCES customer(ssn),
+    CONSTRAINT customer_credit_card_fk FOREIGN KEY (card_no) REFERENCES credit_card(card_no)
 );
 
 CREATE TABLE office(
     office_id INT AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    email VARCHAR(256) UNIQUE NOT NULL,
-    phone_no VARCHAR(256) UNIQUE NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    email VARCHAR(64) UNIQUE NOT NULL,
+    phone_no CHAR(11) UNIQUE NOT NULL,
     password VARCHAR(256) NOT NULL,
-    country VARCHAR(256) NOT NULL,
-    city VARCHAR(256) NOT NULL,
-    building_no VARCHAR(256) NOT NULL,
+    country VARCHAR(64) NOT NULL,
+    city VARCHAR(64) NOT NULL,
+    building_no VARCHAR(16) NOT NULL,
     CONSTRAINT office_pk PRIMARY KEY (office_id)
 );
 
 CREATE TABLE car(
-    plate_id INT,
-    model VARCHAR(256) NOT NULL,
-    make VARCHAR(256) NOT NULL,
+    plate_id VARCHAR(8),
+    model VARCHAR(32) NOT NULL,
+    make VARCHAR(32) NOT NULL,
     year YEAR NOT NULL,
     price REAL NOT NULL,
     office_id INT NOT NULL,
@@ -41,27 +55,29 @@ CREATE TABLE car(
 
 CREATE TABLE reservation(
     reservation_no INT AUTO_INCREMENT,
-    ssn CHAR(14) NOT NULL,
-    plate_id INT NOT NULL,
+    ssn CHAR(6) NOT NULL,
+    plate_id VARCHAR(8) NOT NULL,
     reserve_date DATE NOT NULL,
     pickup_date DATE NOT NULL,
     return_date DATE NOT NULL,
-    payment_date DATE,
+    payment_date DATE DEFAULT NULL,
+    registration_date DATE DEFAULT GETDATE(),
     CONSTRAINT reservation_pk PRIMARY KEY (reservation_no),
     CONSTRAINT reservation_customer_fk FOREIGN KEY (ssn) REFERENCES customer(ssn),
     CONSTRAINT reservation_car_fk FOREIGN KEY (car_id) REFERENCES car(plate_id)
 );
 
 CREATE TABLE car_photos(
-    plate_id INT,
-    photo VARCHAR(512),    
+    plate_id VARCHAR(8),
+    photo VARCHAR(512),
     CONSTRAINT car_photos_pk PRIMARY KEY (plate_id),
     CONSTRAINT car_photos_fk FOREIGN KEY (plate_id) REFERENCES car(plate_id)
 );
 
+--- 0-> available, 1-> in maintainance, 2-> rented, 3-> returned
 CREATE TABLE car_status(
-    plate_id INT,
-    status_code INT,
+    plate_id VARCHAR(8),
+    status_code SMALLINT,
     status_date DATE,
     CONSTRAINT car_status_pk PRIMARY KEY (plate_id),
     CONSTRAINT car_status_fk FOREIGN KEY (plate_id) REFERENCES car(plate_id)
