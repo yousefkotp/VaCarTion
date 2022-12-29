daca08ae86e96da1297926fb23fe59da91cc1713
Mohamed Farid <84376570+MohamedFarid612@users.noreply.github.com>
2022-12-29 18:50:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -101,6 +101,11 @@ app.get("/add-car", authorizeOffice, (req, res) => {
     res.sendFile(__dirname + "/views/add_car.html");
 });
 
+// write a get req for signup-landing
+app.get("/signup-landing", (req, res) => {
+    res.send("bet redirect l hena ghalat");
+});
+
 /*post requests*/
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -168,6 +173,7 @@ app.post("/signin", (req, res) => {
 
 app.post("/signup", (req, res) => {
     //signing up as a customer
+    console.log(req.body);
     let email = req.body.email;
     let password = req.body.password;
     let fName = req.body.fName;
