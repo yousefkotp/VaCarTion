f2879c1b4f0e717e041e1f30ea065d0ba5573d33
Hossam Elshamy <hossamelshamyy@gmail.com>
2022-12-31 00:44:59 +0200

    fix combo box bugs

--- static/js/office_home.js
+++ static/js/office_home.js
@@ -21,15 +21,7 @@ $(document).ready(function () {
             for (var i = 0; i < data.reservations.length; i++) {
                 var reservation = data.reservations[i];
                 //var revenue = reservation.price * diffDays;
-                //get date object for reservation_date, pickup_date, return_date
-                reservation.reserve_date = new Date(reservation.reserve_date);
-                reservation.pickup_date = new Date(reservation.pickup_date);
-                reservation.return_date = new Date(reservation.return_date);
-                //format in yyyy-mm-dd
-                reservation.reserve_date = reservation.reserve_date.getFullYear() + '-' + (reservation.reserve_date.getMonth() + 1) + '-' + reservation.reserve_date.getDate();
-                reservation.pickup_date = reservation.pickup_date.getFullYear() + '-' + (reservation.pickup_date.getMonth() + 1) + '-' + reservation.pickup_date.getDate();
-                reservation.return_date = reservation.return_date.getFullYear() + '-' + (reservation.return_date.getMonth() + 1) + '-' + reservation.return_date.getDate();
-                var row = '<tr><td>' + reservation.reservation_no + '</td><td>' + reservation.plate_id + '</td><td>' + reservation.fname + " " + reservation.lname + '</td><td>' + reservation.reserve_date + '</td><td>' + reservation.pickup_date + '</td><td>' + reservation.return_date+ '</td><td>' + reservation.revenue + '</td></tr>';
+                var row = '<tr><td>' + reservation.reservation_no + '</td><td>' + reservation.plate_id + '</td><td>' + reservation.fname + " " + reservation.lname + '</td><td>' + reservation.reserve_date.substr(0, 10) + '</td><td>' + reservation.pickup_date.substr(0, 10) + '</td><td>' + reservation.return_date.substr(0, 10) + '</td><td>' + reservation.revenue + '</td></tr>';
                 $('.office-reservation').append(row);
             }
         }
@@ -42,54 +34,36 @@ $(document).ready(function () {
             for (var i = 0; i < data.cars.length; i++) {
                 var car = data.cars[i];
                 console.log(car);
-                statuss.unshift(statuss[Number(car.status_code)]);
+                statuss.unshift(refStatuss  [Number(car.status_code)]);
                 var status_codes = statuss.filter((element, index) => {
                     return statuss.indexOf(element) === index;
                 });
-                //convert registration date to date format
-                let date = new Date(car.registration_date);
-                //format in yyyy-mm-dd
-                let formatted_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-                var row = '<tr><td>' + car.plate_id + '</td>' + "<TD> <select>\
-                         <option value=\"saab\">"+ status_codes[0] + "</option>\
-                         <option value=\"volvo\">" + status_codes[1] + "</option>\
-                         <option value=\"mercedes\">"+ status_codes[2] + "</option>\
-                         <option value=\"audi\">"+ status_codes[3] + "</option>\
+                dis = ''
+                if (status_codes[0] == "Rented")
+                dis = "disabled";
+                var row = '<tr><td>' + car.plate_id + '</td>' + "<TD> <select " + dis +">\
+                         <option >"+ status_codes[0] + "</option>\
+                         <option >" + status_codes[1] + "</option>\
+                         <option >"+ status_codes[2] + "</option>\
+                         <option disabled>"+ status_codes[3] + "</option>\
                     </select>\
-                 </TD><td>" + formatted_date + '</td><td>' + car.make + '</td><td>' + car.model + '</td><td>' + car.year + '</td><td>' + car.price +
+                 </TD><td>" + car.registration_date.substr(0, 10) + '</td><td>' + car.make + '</td><td>' + car.model + '</td><td>' + car.year + '</td><td>' + car.price +
                     "<td><button style=\"padding: 0\;border: none\;background: none\; \"><img src=\"/remove.png\" width=25 height=25> </button></td>" + '</td></tr>';
                 $('.office-cars').append(row);
             }
         }
     });
-    //save the original status 
-    var prev;
-    $('.office-cars').on('click', 'select', function (e) {
-        prev = $(this).find(":selected").text();
-    });
 
-    $('.office-cars').on('change', function (e) {
-        if(prev == "Rented"){
-            $(this).find(":selected").text(prev);
-            alertify.notify("Can't change the status of currently rented car", 'error');
-            return;
-        }
-        //get row where change happened
-        var row = $(e.target).closest('tr');
-        //get plate_id
-        var plate_id = row.children('td:first').text();
-        //get new status
+    $('.office-cars').on('change', 'select', function (e) {
         var new_status = refStatuss.indexOf($(this).find(":selected").text());
-        if(new_status == 3){
-            $(this).find(":selected").text(prev);
-            alertify.notify("The office can't rent cars", 'error');            
-            return;
-        }
+        console.log(new_status);
+        console.log($(this).find(":selected").text())
+        console.log($(this).closest('tr').children('td:first').text());
         $.ajax({
             url: "/add-new-status",
             type: "POST",
             data: {
-                plate_id: plate_id,
+                plate_id: $(this).closest('tr').children('td:first').text(),
                 status: new_status
             },
             success: function (response) {
